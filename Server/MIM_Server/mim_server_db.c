c6345ec774d433ae0b819fd9adee0ff46317048f
MichaelXu@deepin <xuchuaner@qq.com>
2017-05-20 15:32:18 +0800

    增加数据库的常用操作，将数据库的初始化独立成模块

--- Server/MIM_Server/mim_server_db.c
+++ Server/MIM_Server/mim_server_db.c
@@ -10,6 +10,8 @@
  * MODIFICATION HISTORY:
  * --------------------------------------
  *      2017-05-15 01:27:32 添加 sSqlChkRet 函数，并设置为static模式
+ *      2017-05-15 11:23:31 sSqlChkRet 对外提供
+ *      2017-05-15 12:21:20 添加数据库（表）的初始化函数
 *****************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
@@ -22,6 +24,184 @@
 
 /*****************************************************************************
  * DECRIPTION:
+ *      初始化用户密码表 USER_PASSWD_TBL
+ * INPUTS:
+ *      sqlite3* sqlHdl 数据库操作对象
+ * OUTPUTS:
+ *      NONE
+ * RETURNS:
+ *      OK      --  成功
+ *      ERROR   --  失败
+ *      INVALID_PARAM --  参数错误
+ * CAUTIONS:
+ *      static
+*****************************************************************************/
+static STATUS sTblUserPasswdInit(sqlite3* sqlHdl)
+{
+    STATUS ret = ERROR;
+    const char *sql = "CREATE TABLE USER_PASSWD_TBL(\n"\
+                "UID    INT     PRIMARY KEY     NOT NULL,\n"\
+                "UNAME  TEXT    KEY             NOT NULL,\n"\
+                "UPASSWD    TEXT                NOT NULL\n"\
+                ");";
+    PRINTF("%s", sql);
+    ret = sqlite3_exec (sqlHdl, sql, 0, 0, 0);
+    sSqlChkRet (sqlHdl, ret, "Init UserPasswd Tbl");
+
+    if (SQLITE_OK == ret)
+    {
+        return OK;
+    }
+    else
+    {
+        return ERROR;
+    }
+}
+
+/*****************************************************************************
+ * DECRIPTION:
+ *      初始化用户基本信息表 USER_INFO_TBL;
+ * INPUTS:
+ *      sqlite3* sqlHdl 数据库操作对象
+ * OUTPUTS:
+ *      NONE
+ * RETURNS:
+ *      OK      --  成功
+ *      ERROR   --  失败
+ *      INVALID_PARAM --  参数错误
+ * CAUTIONS:
+ *      static
+*****************************************************************************/
+static STATUS sTblUserInfoInit(sqlite3* sqlHdl)
+{
+    STATUS ret = ERROR;
+    char *sql = "CREATE TABLE USER_INFO_TBL("
+                "UID    INT     PRIMARY KEY     NOT NULL,\n"
+                "SEX    TEXT    KEY             NOT NULL,\n"
+                "EMAIL  TEXT,   \n"
+                "TEL    TEXT    \n"
+                ");";
+    ret = sqlite3_exec (sqlHdl, sql, 0, 0, 0);
+    sSqlChkRet (sqlHdl, ret, "Init UserInfo Tbl");
+
+    if (SQLITE_OK == ret)
+    {
+        return OK;
+    }
+    else
+    {
+        return ERROR;
+    }
+}
+
+/*****************************************************************************
+ * DECRIPTION:
+ *      初始化用户好友列表 USER_FRDS_TBL;
+ * INPUTS:
+ *      sqlite3* sqlHdl 数据库操作对象
+ * OUTPUTS:
+ *      NONE
+ * RETURNS:
+ *      OK      --  成功
+ *      ERROR   --  失败
+ *      INVALID_PARAM --  参数错误
+ * CAUTIONS:
+ *      static
+*****************************************************************************/
+static STATUS sTblUserFrdsInit(sqlite3* sqlHdl)
+{
+    STATUS ret = ERROR;
+    char *sql = "CREATE TABLE USER_FRDS_TBL("
+                "UID    INT     PRIMARY KEY     NOT NULL,\n"
+                "FID    INT                     NOT NULL,\n"
+                "REMARK  TEXT   \n"
+                ");";
+    ret = sqlite3_exec (sqlHdl, sql, 0, 0, 0);
+    sSqlChkRet (sqlHdl, ret, "Init UserFrds Tbl");
+
+    if (SQLITE_OK == ret)
+    {
+        return OK;
+    }
+    else
+    {
+        return ERROR;
+    }
+}
+
+/*****************************************************************************
+ * DECRIPTION:
+ *      初始化用户在线状态信息表 USER_STAT_TBL;
+ * INPUTS:
+ *      sqlite3* sqlHdl 数据库操作对象
+ * OUTPUTS:
+ *      NONE
+ * RETURNS:
+ *      OK      --  成功
+ *      ERROR   --  失败
+ *      INVALID_PARAM --  参数错误
+ * CAUTIONS:
+ *      static
+*****************************************************************************/
+static STATUS sTblUserStatInit(sqlite3* sqlHdl)
+{
+    STATUS ret = ERROR;
+    char *sql = "CREATE TABLE USER_STAT_TBL("
+                "UID    INT     PRIMARY KEY     NOT NULL,\n"
+                "STAT   TEXT                    NOT NULL,\n"
+                ");";
+    ret = sqlite3_exec (sqlHdl, sql, 0, 0, 0);
+    sSqlChkRet (sqlHdl, ret, "Init UserStat Tbl");
+
+    if (SQLITE_OK == ret)
+    {
+        return OK;
+    }
+    else
+    {
+        return ERROR;
+    }
+}
+
+/*****************************************************************************
+ * DECRIPTION:
+ *      初始化 用户密码重置验证问题表 USER_VERIFY_TBL;
+ * INPUTS:
+ *      sqlite3* sqlHdl 数据库操作对象
+ * OUTPUTS:
+ *      NONE
+ * RETURNS:
+ *      OK      --  成功
+ *      ERROR   --  失败
+ *      INVALID_PARAM --  参数错误
+ * CAUTIONS:
+ *      static
+*****************************************************************************/
+static STATUS sTblUserVerifyInit(sqlite3* sqlHdl)
+{
+    STATUS ret = ERROR;
+    char *sql = "CREATE TABLE USER_VERIFY_TBL("
+                "UID    INT     PRIMARY KEY     NOT NULL,\n"
+                "Q1     TEXT                    NOT NULL,\n"
+                "Q2     TEXT                    NOT NULL,\n"
+                "Q3     TEXT                    NOT NULL,\n"
+                ");";
+    ret = sqlite3_exec (sqlHdl, sql, 0, 0, 0);
+    sSqlChkRet (sqlHdl, ret, "Init UserVerify Tbl");
+
+    if (SQLITE_OK == ret)
+    {
+        return OK;
+    }
+    else
+    {
+        return ERROR;
+    }
+}
+
+
+/*****************************************************************************
+ * DECRIPTION:
  *      检验SQL函数执行结果
  * INPUTS:
  *      sqlite3* sqlHdl --  SQL处理对象
@@ -30,12 +210,13 @@
  * OUTPUTS:
  *      NONE
  * RETURNS:
- *      NONE
+ *      OK (0)       //处理成功
+ *      ERROR (-1)    //处理失败
  * CAUTIONS:
- *      static 静态，只能本文件内使用
 *****************************************************************************/
-static void sSqlChkRet(sqlite3* sqlHdl, STATUS ret, char* curOpera)
+STATUS sSqlChkRet(sqlite3* sqlHdl, STATUS ret, char* curOpera)
 {
+    STATUS stat = ERROR;
     /* 如果返回值不是OK,则输出返回值和警告信息 */
     if (ret != SQLITE_OK)
     {
@@ -43,17 +224,27 @@ static void sSqlChkRet(sqlite3* sqlHdl, STATUS ret, char* curOpera)
         fprintf(stderr, "\nSQL error(%s): %s",
                 curOpera, sqlite3_errmsg(sqlHdl));
         fflush(stderr);
-#ifdef _STRICT_DEBUG
-        EXIT(EXIT_FAILURE);
-#endif
+
+        if (ret == SQLITE_BUSY)
+        {
+            return stat;    //如果是由于数据库繁忙直接返回
+        }
+        else
+        {
+    #ifdef _STRICT_DEBUG
+            EXIT(EXIT_FAILURE); //如果是由于其他原因，结束进程
+    #endif
+        }//end of IF BUSY
     }
     else
     {
 #ifdef _DEBUG
         PRINTF("SQL ok(%s).", curOpera);
 #endif
-    }
-    return ;
+        stat = OK;
+    }//end of IF NOT OK
+
+    return stat;
 }
 
 /*****************************************************************************
@@ -62,21 +253,18 @@ static void sSqlChkRet(sqlite3* sqlHdl, STATUS ret, char* curOpera)
  * INPUTS:
  *      数据库文件名 dbFileName
  * OUTPUTS:
- *      打开的数据库操作对象 sqlite3 **sqlHdl
+ *      打开的数据库操作对象 sqlite3 *sqlHdl
  * RETURNS:
- *      OK      --  成功
- *      ERROR   --  失败
- *      INVALID_PARAM --  参数错误
+ *      NONE
  * CAUTIONS:
  *      如果不在当前文件夹下创建数据库，需要提供完整
  *      路径，最好使用绝对路径
 *****************************************************************************/
-STATUS sDbOpen(const char *dbFileName, sqlite3 **sqlHdl)
+STATUS sDbOpen(const char *dbFileName, sqlite3 *sqlHdl)
 {
     STATUS ret = SQLITE_ERROR;
 
-    ret = sqlite3_open (dbFileName, sqlHdl);
-    sSqlChkRet (*sqlHdl, ret, "Open DB");
+    ret = sqlite3_open (dbFileName, &sqlHdl);
 
     return ret;
 }
@@ -103,5 +291,61 @@ STATUS sDbClose(sqlite3 *sqlHdl)
     ret = sqlite3_close(sqlHdl);
     sSqlChkRet (sqlHdl, ret, "Close DB");
     PRINTF ("\n");
+    return ret;
+}
+
+/*****************************************************************************
+ * DECRIPTION:
+ *      sDbInit()   初始化数据库：初始化所有表
+ * INPUTS:
+ *      sqlite3* sqlHdl;    数据库操作对象
+ * OUTPUTS:
+ *      NONE
+ * RETURNS:
+ *      OK      --  成功
+ *      ERROR   --  失败
+ *      INVALID_PARAM --  参数错误
+ * CAUTIONS:
+ *      NONE
+*****************************************************************************/
+STATUS sDbInit(sqlite3* sqlHdl)
+{
+    STATUS ret = ERROR;
+
+    /* 初始化 用户名密码表 */
+    ret = sTblUserPasswdInit(sqlHdl);
+    if (ERROR == ret)
+    {
+        goto o_exit;
+    }
+
+    /* 初始化 用户信息表 */
+    ret = sTblUserInfoInit (sqlHdl);
+    if (ERROR == ret)
+    {
+        goto o_exit;
+    }
+
+    /* 初始化用户好友列表 */
+    ret = sTblUserFrdsInit (sqlHdl);
+    if (ERROR == ret)
+    {
+        goto o_exit;
+    }
+
+    /* 初始化用户在线状态信息表 */
+    ret = sTblUserStatInit (sqlHdl);
+    if (ERROR == ret)
+    {
+        goto o_exit;
+    }
+
+    /* 初始化 用户密码重置验证问题表 */
+    ret = sTblUserVerifyInit (sqlHdl);
+    if (ERROR == ret)
+    {
+        goto o_exit;
+    }
+o_exit:
     return ret;
 }
--- Server/MIM_Server/mim_server_db.h
+++ Server/MIM_Server/mim_server_db.h
@@ -11,14 +11,33 @@
  * --------------------------------------
  *      2017-05-15 00:11:10 添加 sSqlChkRet 函数，处理SQL操作结果
  *      2017-05-15 01:26:06 设置 sSqlChkRet函数为static，并直接定义在mim_server_db.c
+ *      2017-05-15 11:21:49 重新设置sSqlChkRet函数为非static，对外提供接口
 *****************************************************************************/
 
 #ifndef MIM_SERVER_DB_H
 #define MIM_SERVER_DB_H
 
+#define DB_FILE "users.db"
+
 #include <sqlite3.h>
 #include "mim_sc_common.h"
 
+
+/*****************************************************************************
+ * DECRIPTION:
+ *      检验SQL函数执行结果
+ * INPUTS:
+ *      sqlite3* sqlHdl --  SQL处理对象
+ *      STATUS ret --  相应的SQL函数返回值
+ *      char* curOpera --   当前执行的操作名字
+ * OUTPUTS:
+ *      OK (0)       结果正常
+ *      ERROR (-1)   结果异常
+ * RETURNS:
+ *      NONE
+ * CAUTIONS:
+*****************************************************************************/
+STATUS sSqlChkRet(sqlite3* sqlHdl, STATUS ret, char* curOpera);
 
 /*****************************************************************************
  * DECRIPTION:
@@ -32,10 +51,11 @@
  *      ERROR   --  失败
  *      INVALID_PARAM --  参数错误
  * CAUTIONS:
- *      如果不在当前文件夹下创建数据库，需要提供完整
- *      路径，最好使用绝对路径
+ *      1. 如果不在当前文件夹下创建数据库，需要提供完整
+ *      路径，最好使用绝对路径;
+ *      2. 如果打开失败直接结束进程;
 *********************************************/
-STATUS sDbOpen(const char *dbFileName, sqlite3 **sqlHdl);
+STATUS sDbOpen(const char *dbFileName, sqlite3 *sqlHdl);
 
 /***********************************************
  * DECRIPTION:
@@ -53,5 +73,38 @@ STATUS sDbOpen(const char *dbFileName, sqlite3 **sqlHdl);
  *      路径，最好使用绝对路径
 *****************************************************************************/
 STATUS sDbClose(sqlite3 *sqlHdl);
+
+/*****************************************************************************
+ * DECRIPTION:
+ *      sDbCreateTbl()创建表
+ * INPUTS:
+ *      char* tblName   --  表名
+ *
+ * OUTPUTS:
+ *      NONE
+ * RETURNS:
+ *      OK      --  成功
+ *      ERROR   --  失败
+ *      INVALID_PARAM --  参数错误
+ * CAUTIONS:
+ *      NONE
+*****************************************************************************/
+
+
+/*****************************************************************************
+ * DECRIPTION:
+ *      sDbInit()   初始化数据库：初始化所有表
+ * INPUTS:
+ *      sqlite3* sqlHdl; 数据库对象
+ * OUTPUTS:
+ *      NONE
+ * RETURNS:
+ *      OK      --  成功
+ *      ERROR   --  失败
+ *      INVALID_PARAM --  参数错误
+ * CAUTIONS:
+ *      NONE
+*****************************************************************************/
+STATUS sDbInit(sqlite3* sqlHdl);
 
 #endif // MIM_SERVER_H
