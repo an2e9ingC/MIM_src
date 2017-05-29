afb5b390f9edf953357e2ad24aa58cc80d358817
MichaelXu@deepin <xuchuaner@qq.com>
2017-05-30 00:02:34 +0800

    经过测试， 多线程可以实现，接下来需要在 线程处理函数中完成客户请求的处理

--- Server/MIM_Server/main.c
+++ Server/MIM_Server/main.c
@@ -9,7 +9,8 @@
  *      2017.05.09
  * MODIFICATION HISTORY:
  * --------------------------------------
- *
+ *      2017-05-30 00:03:44 基本实现多线程功能，但是具体的客户请求处理的核心函数
+ *      serverWork需要具体完善
 *****************************************************************************/
 
 #include <stdio.h>
@@ -33,12 +34,40 @@
 sqlite3 *sqlHdl = NULL; //SQL处理对象指针
 sqlite3** pSqlHdl = &sqlHdl;    //二级指针
 
-void newThreadWork(void)
+/*****************************************************************************
+ * DECRIPTION:
+ *      serverWork() 新客户端连接后的核心处理函数
+ *      负责处理用户请求
+ * INPUTS:
+ *      NONE
+ * OUTPUTS:
+ *      NONE
+ * RETURNS:
+ *      OK      --  成功
+ *      ERROR   --  失败
+ *      INVALID_PARAM --  参数错误
+ * CAUTIONS:
+ *      NONE
+*****************************************************************************/
+void serverWork(void)
 {
+    ssize_t realRead, realWrite;    //实际接收到的和发送的数据大小
+    char buf[BUF_SIZE] = {'\0'};    //暂存接收和发送的数据buf
+
+
     PRINTF("this is a new thread = %lu.", pthread_self ());
     return ;
 }
 
+/*****************************************************************************
+ * DECRIPTION:
+ *      main 函数  程序入口
+ * FUNCTIONS:
+ *      1. 数据库的初始化；
+ *      2. 套接字的建立
+ *      3. 多线程的建立
+ * 具体的客户端连接后的处理操作全部放在 serverWork() 函数中
+*****************************************************************************/
 int main()
 {
     STATUS ret = ERROR;
@@ -86,8 +115,6 @@ int main()
     SAIN servAddr, clientAddr;  //定义服务器和客户端的地址结构体
     int listenfd, connfd;   //监听、连接套接字
     int clientAddrLen = sizeof(clientAddr);  //客户端socket地址大小
-    ssize_t realRead, realWrite;    //实际接收到的和发送的数据大小
-    char buf[BUF_SIZE] = {'\0'};    //暂存接收和发送的数据buf
 
     /* 1. 创建套接字 */
     listenfd = socketCreate (AF_INET, SOCK_STREAM, 0);   //创建监听套接字,tcp,流式
@@ -116,17 +143,18 @@ int main()
             continue;   //继续loop， 知道有connect
         }
 
-        //当有新的客户端连接后，创建新的线程进行服务
+        //当有新的客户端连接后，创建新的线程为其进行服务
         PRINTF("Got a New connection. connfd = %d.", connfd);
-        if(-1 == pthread_create(&newThread, NULL, (void*)(&newThreadWork), NULL))
+        if(-1 == pthread_create(&newThread, NULL, (void*)(&serverWork), NULL))
         {
             PRINTF("New Thread Created Failed.");
         }
+    }//end of while
 
-
+    if ( -1 != socketShutdown (listenfd, SHUT_RDWR))
+    {
+        PRINTF("[socket shutdown...]");
     }
-
-
 
 
     /* 关闭数据库 */
